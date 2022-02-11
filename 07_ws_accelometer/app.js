1e00498b0f5e8cd0e943ab17c54c7ccf038c66eb
axuboi <aleksanteri.mustajarvi@seamk.fi>
2022-02-11 18:04:11 +0200

    Added 07

---
+++ 07_ws_accelometer/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+const PORT = process.env.PORT || 8081;
+
+let connections = [];
+
+io.on("connection", (socket)=>{
+    connections[socket.id] = socket;
+    console.log("connected");
+
+    socket.on("disconnect", (socket)=>{
+        delete connections[socket.id];
+        console.log("disconnected");
+    });
+
+    socket.on("sensor_data", (data)=>{
+        console.log(data);
+    })
+});
+
+// localhost:8081/abc.txt -> this is fetched from /public folder.
+app.use(express.static("public"));
+
+server.listen(PORT);
