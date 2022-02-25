e21306a249595a482ca6ee4688ddc07c0f5289f9
axuboi <aleksanteri.mustajarvi@seamk.fi>
2022-02-25 20:02:54 +0200

    commit

--- 10_fanuc_viewer/public/client.js
+++ 10_fanuc_viewer/public/client.js
@@ -1,7 +1,19 @@
 import * as THREE from "three"; // Also installed three "npm install three".
 
-const renderer = THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
+
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(75, aspect);
 renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const render = ()=>{
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+render();
 
 let socket = io("https://fanuc-ws-server-albert.herokuapp.com/", {withCredentials: false});
 
