85ff651fd2f5f2e5177318e0d5e97d5ab023aaf0
axuboi <aleksanteri.mustajarvi@seamk.fi>
2022-02-25 19:53:25 +0200

    moved files

--- 10_fanuc_viewer/public/client.js
+++ 10_fanuc_viewer/public/client.js
@@ -1,3 +1,8 @@
+import * as THREE from "three"; // Also installed three "npm install three".
+
+const renderer = THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+
 let socket = io("https://fanuc-ws-server-albert.herokuapp.com/", {withCredentials: false});
 
 socket.on("joint_values", (joint_values)=>{
