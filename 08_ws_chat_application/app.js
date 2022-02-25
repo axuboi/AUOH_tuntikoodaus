f47976cba0277e37a2e15fb61526dfad9d3cd7c8
axuboi <aleksanteri.mustajarvi@seamk.fi>
2022-02-25 17:54:46 +0200

    commit 08 working chat

--- 08_ws_chat_application/app.js
+++ 08_ws_chat_application/app.js
@@ -6,33 +6,60 @@ const io = require("socket.io")(server); // remember to npm install socket.io
 const PORT = process.env.port || 8081;
 
 let connections = [];
+let nicknames = [];
 let number_of_connections = 0;
 
 const broadcast_message = (message)=>{
     for(let id in connections){
         const socket = connections[id];
-        socket.emit("server-to-client", message);
+        socket.emit("new_message", message);
     }
+};
+
+const broadcast_n_clients = ()=>{
+    for(let id in connections){
+        const socket = connections[id];
+        socket.emit("n_clients", number_of_connections);
+    }
+};
+
+const emit_nickname = (socket)=>{
+    socket.emit("nickname", nicknames[socket.id]);
 }
 
 // handle the web socket
 io.on("connection", (socket)=>{
     connections[socket.id] = socket;
+
+    nicknames[socket.id] = socket.id; // set default nickname
+    emit_nickname(socket);
+
     number_of_connections++;
+    broadcast_n_clients();
 
     //socket.emit("server-to-client", "Hello from server!");
 
     console.log("Client connected. Number of connections: ", number_of_connections);
+
     socket.on("disconnect", (socket)=>{
         delete connections[socket.id];
         number_of_connections--;
+        broadcast_n_clients();
         console.log("Client disconnected. Number of connections: ", number_of_connections); 
     });
     
-    socket.on("client-to-server", (message)=>{
+    socket.on("send_message", (text)=>{
+        const message = {
+            nickname: nicknames[socket.id],
+            text: text
+        }
         broadcast_message(message);
-        //console.log(message);
-    })
+    });
+
+    socket.on("set_nickname", (nickname)=>{
+        nicknames[socket.id] = nickname;
+        emit_nickname(socket);
+    });
 
 })
 
--- 08_ws_chat_application/public/client.js
+++ 08_ws_chat_application/public/client.js
@@ -1,14 +1,37 @@
 let socket = io();
 
+socket.on("new_message", (message)=>{
+    const messages = document.getElementById("messages");
+
+    const nickname = message.nickname;
+    const text = message.text;
+    
+    messages.innerHTML += "<b>" + nickname + ":</b> " + text + "<br>";
+})
+
 socket.on("server-to-client", (message)=>{
     const messages = document.getElementById("messages");
     messages.innerHTML += message + "<br>";
-    //console.log(message);
-    //socket.emit("client-to-server", "Hello from client!");
 });
+
+socket.on("n_clients", (n_clients)=>{
+    const element = document.getElementById("n_clients"); // n_clients is the HTML element in index.html
+    element.innerHTML = "Number of clients: " + n_clients;
+});
+
+socket.on("nickname", (nickname)=>{
+    const element = document.getElementById("nickname");
+    element.innerHTML = "Nickname: " + nickname;
+});
+
+const set_nickname = ()=>{
+    const nickname_input = document.getElementById("nickname_input");
+    socket.emit("set_nickname", nickname_input.value);
+    message_input.value = "";
+}
 
 const send_message = ()=>{
     const message_id = document.getElementById("message_input");
-    socket.emit("client-to-server", message_input.value);
+    socket.emit("send_message", message_input.value);
     message_input.value = "";
 }
